ad10b8660eab3eeb7d3a755e01480a4481c078ae
Hassana  Abdullahi <hassanaabdll1@gmail.com>
2022-10-09 19:20:10 +0100

    Updated index.js

--- week1/index.js
+++ week1/index.js
@@ -2,7 +2,9 @@
 
 //#1 // Comments can make code readable
 //#2 // Welcome to 30DaysOfJavaScript
-//#3 // comments can make code readable, easy to reuse and informative
+//#3 /*comments can make code readable, 
+easy to reuse and
+ informative*/
 
 //#4 declarering variables.
 
